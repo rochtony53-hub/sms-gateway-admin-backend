3acaea1871159fee3ae741bf86bc95e714629267
randrianarivera67-arch <randrianarivera67@gmail.com>
2026-06-11 17:53:41 +0300

    fix: apikey middleware - prevent undefined bypass

--- middleware/apikey.js
+++ middleware/apikey.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   const key = req.headers['x-api-key'];
-  if (key !== process.env.API_KEY)
+  if (!key || !process.env.API_KEY || key !== process.env.API_KEY)
     return res.status(403).json({ error: 'API key invalide' });
   next();
 };
